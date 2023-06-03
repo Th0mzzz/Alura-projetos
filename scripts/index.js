b4a7f2af2b569b12e4b860021e4ec742fece513f
Thomaz <rm86681@estudante.fieb.edu.br>
2023-06-03 04:19:54 -0300

    att

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,44 @@
 let formBtn = document.querySelector(".form-btn");
 let setaList = document.querySelector(".seta-list");
 let formContainer = document.querySelector(".form-list__container");
+let formItens = document.querySelectorAll(".form-list__item");
 
 formBtn.addEventListener('click', formList)
-
+formContainer.addEventListener('mouseleave',leaveFormList)
 function formList (){
+        if(setaList.classList.contains("clicked")){
+            setaList.classList.remove('clicked')
+            formContainer.classList.remove("abrir")
+            formContainer.classList.add('fechar')
+    
+            setTimeout(function(){
+                formContainer.classList.remove("fechar")
+                formContainer.style.height = "0px"
+                
+            },1000);
 
-    if(setaList.classList.contains('clicked')){
-        formContainer.classList.add('fechado');
-        formContainer.classList.remove("aberto")
-        setaList.classList.remove("clicked");
+        }else{
+            setaList.classList.add('clicked')
+            formContainer.classList.add('abrir')
+        
 
-    }else{
-        formContainer.classList.remove('fechado');
-        formContainer.classList.add('aberto')
-        setaList.classList.add("clicked");
+            setTimeout(function(){
+                formContainer.classList.remove("abrir")
+                formContainer.style.height = "auto"
+            },1000);
+        }
+
+       
     }
-
-
+      
+function leaveFormList(){
+    setaList.classList.remove('clicked')
+            formContainer.classList.remove("abrir")
+            formContainer.classList.add('fechar')
+    
+            setTimeout(function(){
+                formContainer.classList.remove("fechar")
+                formContainer.style.height = "0px"
+                
+            },1000);
 }
